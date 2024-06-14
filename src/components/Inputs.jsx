646aecbd8dd79e509a31037e6b003d596af840e5
lakshmivenkateswara <lakshmivenkateswarapunnam@gmail.com>
2024-06-14 15:15:47 +0530

    wejkwhke

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -23,7 +23,7 @@ function Inputs({ setQuery, units, setUnits }) {
         let lon = position.coords.longitude;
 
         setQuery({
-          
+      
           lat,
           lon,
         });
